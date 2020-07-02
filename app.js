98ad376537271de80e499052cc6a8010d6abe9b2
John Adams <adamsjr8576@gmail.com>
2020-07-01 21:29:44 -0600

    Write tests and pass them for first to GET endpoints

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
   const environment = process.env.NODE_ENV || 'development';
-  console.log(environment)
   const configuration = require('./knexfile')[environment];
   const database = require('knex')(configuration);
 
@@ -19,17 +18,13 @@ app.get('/api/v1/users/:username/:password', async (request, response) => {
 
   try {
     const user = await database('users').where('username', username).select();
-
     if (!user.length) {
       return response.status(404).json({ error: `username: ${username} does not exist. Please try a different username or create an account` })
     }
-
     if (user[0].password !== password) {
-      return response.status(404).json({ error: `The password entered is incorrect. Please try again.`})
+      return response.status(403).json({ error: `The password entered is incorrect. Please try again.`})
     }
-
     const userToSend = [{ id: user[0].id, username: user[0].username, city: user[0].city, state: user[0].state}]
-
     return response.status(200).json(userToSend);
   } catch {
     return response.status(500).json({ error });
@@ -38,14 +33,12 @@ app.get('/api/v1/users/:username/:password', async (request, response) => {
 
 app.get('/api/v1/categories/:id', async (request, response) => {
   const { id } = request.params;
-
+  
   try {
     const categories = await database('categories').where('user_id', id).select();
-
     if (!categories.length) {
       return response.status(404).json({ error: `You do not currently have any categories. Create some!` })
     }
-
     return response.status(200).json(categories);
   } catch {
     return response.status(500).json({ error });
--- app.test.js
+++ app.test.js
@@ -14,6 +14,59 @@ describe('API', () => {
     it('Should return a 200 status at original url', async () => {
       const response = await request(app).get('/');
       expect(response.status).toBe(200);
+    });
+  });
+
+  describe('GET /api/v1/users/:username/:password', async () => {
+    it('Should return 200 with the user information if username and password are correct', async () => {
+      const expectedUser = await database('users').where('username', 'adamsjr8576').select();
+
+      const response = await request(app).get('/api/v1/users/adamsjr8576/test');
+      const user = response.body;
+
+      expect(response.status).toBe(200);
+      expect(user[0].id).toEqual(expectedUser[0].id);
+    });
+
+    it('Should return 404 and an error object stating the username does not exist', async () => {
+      const invalidUsername = 'adamsjr';
+
+      const response = await request(app).get(`/api/v1/users/${invalidUsername}/test`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual(`username: adamsjr does not exist. Please try a different username or create an account`)
+    });
+
+    it('Should return 403 and an error object stating the password is incorrect', async () => {
+      const invalidPassword = 'wrong';
+
+      const response = await request(app).get(`/api/v1/users/adamsjr8576/${invalidPassword}`);
+
+      expect(response.status).toBe(403);
+      expect(response.body.error).toEqual(`The password entered is incorrect. Please try again.`);
+    });
+  });
+
+  describe('GET /api/v1/categories/:id', async () => {
+    it('Should return 200 with the category objects based on a user ID', async () => {
+      const expectedUser = await database('users').first();
+      const { id } = expectedUser;
+      const expectedCategories = await database('categories').where('user_id', id);
+
+      const response = await request(app).get(`/api/v1/categories/${id}`);
+      const category = response.body[0];
+
+      expect(response.status).toBe(200);
+      expect(category.id).toEqual(expectedCategories[0].id)
+    });
+
+    it('Should return a 404 if no categories are found', async () => {
+      const invalidUserId = 10;
+
+      const response = await request(app).get(`/api/v1/categories/${invalidUserId}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual('You do not currently have any categories. Create some!');
     })
-  })
+  });
 })
