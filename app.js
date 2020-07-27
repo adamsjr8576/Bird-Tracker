5e0b9b7b422a6bf82180c37d1537abb8956a984b
John Adams <adamsjr8576@gmail.com>
2020-07-27 14:19:11 -0600

    add Procfile

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors = require('cors');
   const database = require('knex')(configuration);
 
 app.locals.title = 'Bird Tracker';
-app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(cors());
 
